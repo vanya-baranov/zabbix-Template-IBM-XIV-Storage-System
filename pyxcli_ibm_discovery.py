d2d31c75ca678b35ae9b8e6b7684636664ffa591
vanya-baranov <93309680+vanya-baranov@users.noreply.github.com>
2026-03-13 14:22:10 +0300

    Update pyxcli_ibm_discovery.py

--- pyxcli_ibm_discovery.py
+++ pyxcli_ibm_discovery.py
@@ -1,81 +1,28 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*
-
+#  Test Ok on python 3.11
 from pyxcli.client import XCLIClient
 import json, sys
-
-
-# Variable Arguments
 
 ibm_user = sys.argv[1]
 ibm_pass = sys.argv[2]
 ibm_host_ip = sys.argv[3]
 
-# Connect SSL to XIV - Storage
+# подключение (timeout уже внутри клиента)
 xcli_client = XCLIClient.connect_ssl(ibm_user, ibm_pass, ibm_host_ip)
 
 result_dict = {}
 
+# команды, которые вы хотите вызвать
+commands = [
+    'ups_list', 'ats_list', 'cna_list', 'cpu_list', 'dimm_list',
+    'disk_list', 'fan_list', 'fc_port_list', 'mm_list', 'psu_list',
+    'switch_list', 'pool_list', 'mirror_list'
+]
 
-# CMD command and create data lists for parsing
+for cmd in commands:
+    try:
+        result_dict[cmd.replace('_list','')] = getattr(xcli_client.cmd, cmd)().as_list
+    except Exception:
+        result_dict[cmd.replace('_list','')] = []
 
-ups = xcli_client.cmd.ups_list().as_list
-
-result_dict ['ups'] = ups
-
-ats = xcli_client.cmd.ats_list().as_list
-
-result_dict ['ats'] = ats
-
-cna = xcli_client.cmd.cna_list().as_list
-
-result_dict ['cna'] = cna
-
-cpu = xcli_client.cmd.cpu_list().as_list
-
-result_dict ['cpu'] = cpu
-
-dimm = xcli_client.cmd.dimm_list().as_list
-
-result_dict ['dimm'] = dimm
-
-disk = xcli_client.cmd.disk_list().as_list
-
-result_dict ['disk'] = disk
-
-fan = xcli_client.cmd.fan_list().as_list
-
-result_dict ['fan'] = fan
-
-fc_port = xcli_client.cmd.fc_port_list().as_list
-
-result_dict ['fc_port'] = fc_port
-
-mm = xcli_client.cmd.mm_list().as_list
-
-result_dict ['mm'] = mm
-
-psu = xcli_client.cmd.psu_list().as_list
-
-result_dict ['psu'] = psu
-
-switch = xcli_client.cmd.switch_list().as_list
-
-result_dict ['switch'] = switch
-
-pool = xcli_client.cmd.pool_list().as_list
-
-result_dict ['pool'] = pool
-
-mirror = xcli_client.cmd.mirror_list().as_list
-
-result_dict ['mirror'] = mirror
-
-
-#Translation of data into json format
-result_dict_json = json.dumps(result_dict)
-
-try:
-    print(result_dict_json)
-except:
-    print(1)
+print(json.dumps(result_dict, indent=2))
